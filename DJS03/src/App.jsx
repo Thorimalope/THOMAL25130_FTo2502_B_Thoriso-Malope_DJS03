09e081ac7ceaf45e22ab123d831f7d14a904becc
Thoriso Malope <thorimalope@gmail.com>
2025-07-06 14:32:22 +0200

    updates on the app component

--- DJS03/src/App.jsx
+++ DJS03/src/App.jsx
@@ -1,20 +1,26 @@
+import React, { useState, useEffect } from "react"; 
 import Header from "./Header.jsx"
 import PodcastCard from "./PodcastCards.jsx"
+import { genres } from "./Data.js"
 import "./index.css"
 
 
 export default function App() {
+
+    const [podcasts, setPodcasts] = useState([]);
+
+    useEffect(function () {
+        fetch("https://podcast-api.netlify.app/")
+    })
+
+    const genreData = genres.map(function (genre) {
+        return <PodcastCard genre={genre.title} key={genre.id} />
+    })
     return (
         <>
             <Header></Header>
             <main id="podcast-grid" className="grid">
-                <PodcastCard />
-                <PodcastCard />
-                <PodcastCard />
-                <PodcastCard />
-                <PodcastCard />
-                <PodcastCard />
-                <PodcastCard />
+                {genreData}
             </main>
         </>    
     )
--- DJS03/src/PodcastCards.jsx
+++ DJS03/src/PodcastCards.jsx
@@ -1,10 +1,10 @@
-export default function PodcastCard () {
+export default function PodcastCard (props) {
     return (
     <div className="card">
         <img className="poster" />
         <h3 className="title"></h3>
         <p className="season-count"></p>
-        <div className="tags"></div>
+        <div className="tags">{props.title}</div>
         <p className="updated-text"></p>
   </div>
     )
